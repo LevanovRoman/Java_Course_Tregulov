c2d5c59a5f8905296be20b60c4075bf816784729
levanovroman <levanovroman2016@yandex.ru>
2024-02-12 11:53:35 +0300

    work with files

---
+++ src/work_with_files/RandomAccessFileEx.java
@@ -0,0 +1,33 @@
+package work_with_files;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileEx {
+    public static void main(String[] args) {
+        try(RandomAccessFile file = new RandomAccessFile("test10.txt", "rw")){
+            int a = file.read();
+            System.out.println((char) a);
+            String s1 = file.readLine();
+            System.out.println(s1);
+
+//            установить курсор на позицию 101
+            file.seek(101);
+            String s2 = file.readLine();
+            System.out.println(s2);
+//            найти позицию курсора
+            long position = file.getFilePointer();
+            System.out.println(position);
+
+//            file.seek(0);
+//            file.writeBytes("privet");
+
+            file.seek(file.length());
+            file.writeBytes("\n\t\t\t\t\tWilliam Yeats");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/work_with_files/programmer2/SerializationEx1.java
@@ -0,0 +1,20 @@
+package work_with_files.programmer2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+public class SerializationEx1 {
+    public static void main(String[] args) {
+        ArrayList employees;
+        try(ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("employees1.bin")))
+        {
+            employees = (ArrayList) inputStream.readObject();
+            System.out.println(employees);
+        } catch (ClassNotFoundException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
